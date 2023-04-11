49c14408d97ce984b5bab21db241ead39531f767
Kyeongoh-Ki <56759587+casentino@users.noreply.github.com>
2023-04-11 10:54:07 +0900

    user, todo router (#3)

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import cors from 'cors';
 import express from 'express';
+import userRouter from './routes/userRoutes';
+import todoRouter from './routes/todoRoutes';
 
 const app = express();
 
@@ -7,6 +9,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
+
+app.use('/user', userRouter);
+app.use('/todo', todoRouter);
 
 app.use((req, res, next) => {
 	res.status(404);
