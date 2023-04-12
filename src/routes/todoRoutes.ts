759ad5e67f8a0b065ae166e0f9e2bc9ac86643aa
casentino <opterk@gmail.com>
2023-04-13 05:12:17 +0900

    type 수정, import 변경

--- src/routes/todoRoutes.ts
+++ src/routes/todoRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import * as todoController from '../controller/todoController';
+import { todoController } from '../controller';
 const router = express.Router();
 
 router.get('/', todoController.getTodos);
